948c5e80390635d07c04c7759fb68774d41d2af5
Guillermo Garcia Amoni <webcolap@gmail.com>
2024-06-01 09:57:37 +0200

    Trabajo Resubido

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-// Función para calcular el descuento
-function calcularDescuento(precio, porcentajeDescuento) {
-    return precio * (porcentajeDescuento / 100);
+function calcularIMC(altura, peso) {
+    return peso / (altura * altura);
 }
 
-// Función para calcular el precio con descuento
-function precioConDescuento(precio, descuento) {
-    return precio - descuento;
+function crearPersona(nombre, genero, peso, altura) {
+    return {
+        nombre: nombre,
+        genero: genero,
+        peso: peso,
+        altura: altura,
+        imc: 0  
+    };
 }
 
-// Función para calcular el impuesto
-function calcularImpuesto(precio, porcentajeImpuesto) {
-    return precio * (porcentajeImpuesto / 100);
+function solicitarDatosYCalcularIMC() {
+    let nombre = prompt("¿Cómo te llamas?");
+    let genero = prompt("¿Eres chico o chica?");
+    let altura = parseFloat(prompt("¿Cuánto mides en metros? (Ej: 1.75)"));
+    let peso = parseFloat(prompt("¿Cuánto pesas en kilogramos?"));
+
+    let persona = crearPersona(nombre, genero, peso, altura);
+    persona.imc = calcularIMC(persona.altura, persona.peso);
+
+    alert(`${persona.nombre}, tu Índice de Masa Corporal (IMC) es ${persona.imc.toFixed(2)}`);
 }
 
-// Función para calcular el precio con impuesto
-function precioConImpuesto(precio, impuesto) {
-    return precio + impuesto;
+alert("¿Quieres calcular tu Índice de Masa Corporal?");
+let respuesta1 = prompt("¡GENIAL! Escriba SI o NO para continuar.");
+
+while (respuesta1.toLowerCase() === "si") {
+    alert("A continuación se le pedirán los datos necesarios");
+    solicitarDatosYCalcularIMC();
+
+    respuesta1 = prompt("¿Quieres calcular el IMC de otra persona? Escriba SI o NO para continuar.");
 }
-// en uso
-const precioProducto = parseFloat(prompt("Ingrese el precio del producto o servicio:"));
-const porcentajeDescuento = parseFloat(prompt("Ingrese el porcentaje de descuento aplicable:"));
-const porcentajeImpuesto = parseFloat(prompt("Ingrese el porcentaje de impuesto aplicable:"));
 
-const descuento = calcularDescuento(precioProducto, porcentajeDescuento);
-const precioFinalConDescuento = precioConDescuento(precioProducto, descuento);
-console.log("El precio con descuento es: $" + precioFinalConDescuento.toFixed(2));
-
-const impuesto = calcularImpuesto(precioFinalConDescuento, porcentajeImpuesto);
-const precioFinalConImpuesto = precioConImpuesto(precioFinalConDescuento, impuesto);
-console.log("El precio final con impuesto es: $" + precioFinalConImpuesto.toFixed(2));
+if (respuesta1.toLowerCase() === "no") {
+    alert("Vaya, es una pena ;(");
+} else {
+    alert("Por favor ingrese una respuesta válida.");
+}
